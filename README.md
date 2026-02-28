4f56721699e4e84d6a3f5542150bd7453e9378b6
Unwize <aaronsigaldev@gmail.com>
2026-02-28 11:38:02 -0800

    Added to readme, added structs for config and db

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # jam-doc-rs
 Analyze the quality of a music library and recommend improvements
+
+## Commands
+
+### `lib`
+
+Lists all registered libraries of music.
+
+- `add <path>`
+  - Adds a path as a library
+- `remove <path>`
+  - Removes a path a library
+
+### `index`
+
+Indexes all music files in each library, asynchronously
+
+  - `purge`
+    - Purges the index
+  
+### `diagnose`
+
+Lists all available diagnostic criteria
+
+  - `bitrate <floor>`
+    - Lists paths for all tracks with bitrates < `floor`
+  - `tagver <v>`
+    - Lists paths for all tracks with id3 tag version != `v`
+  - `incomplete <field0>, <field1>...<fieldN>`
+    - Lists paths for all tracks missing one or more of the specified fields.
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,43 @@
 use anyhow::Result;
 use std::env;
+use rusqlite::{Connection};
+use serde::Deserialize;
 
+/// Representation of the structure of the config file
+#[derive(Deserialize)]
+struct Config {
+    db_path: String
+}
+
+/// Model for tracks in the db
+#[derive(Debug, Clone)]
+struct Track {
+    id: u32,
+    path: String,
+    title: Option<String>,
+    artist: Option<String>,
+    album: Option<String>,
+    bitrate: u32,
+    tagver: Option<String>
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
+    let conn = Connection::open_in_memory()?;
+
+    // Set up track table in DB
+    conn.execute(
+        "CREATE TABLE track (
+            id INTEGER PRIMARY KEY,
+            path STRING NOT NULL,
+            title STRING,
+            artist STRING,
+            album STRING,
+            bitrate INTEGER NOT NULL,
+            tagver STRING
+        )",
+        (),
+    )?;
     Ok(())
 }
